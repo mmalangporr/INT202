8f9b0d460a225ae83357f230b0c25588ec55a14c
TaloeyToey <toey_and_guy@hotmail.com>
2018-11-16 22:37:34 +0700

    Delete Method

--- SWprocess/src/main/java/int202/SWprocess/controller/ProductController.java
+++ SWprocess/src/main/java/int202/SWprocess/controller/ProductController.java
@@ -27,31 +27,11 @@ public class ProductController {
     @Autowired
     private ProductService productService;
     
-//    @GetMapping("/product/{id}")
-//    public String getProductById(@PathVariable long id){
-//        return "this is product"+id;
-//    }
-    
     @GetMapping("/index")
     public String getAllProduct(ModelMap model){
         model.addAttribute("allProduct",productService.getAllProducts());
         return "index";
-    }
-    
-
-//    @RequestMapping("/index")
-//    public long getProductDetail(@RequestParam long productId){
-//       productService.getProductById(productId);
-//       return productId;
-//    }
-    
-    
-   
-//    @RequestMapping("/index")
-//    public String search(@RequestParam String search){
-//        productService.getAllSearch(search);
-//        return search;
-//    }
+    }  
 
     @GetMapping("/search")
     public String searchProduct(ModelMap model,@RequestParam String search){
@@ -61,8 +41,7 @@ public class ProductController {
     
     @RequestMapping("/search")
     public String searchPro(@RequestParam String search){
-        productService.getAllSearch(search);
-        
+        productService.getAllSearch(search); 
         return search;
     }
     
@@ -77,18 +56,11 @@ public class ProductController {
         model.addAttribute("productDetail",productService.getProductById(id));
         return "productdetail";
     }
-<<<<<<< Updated upstream
-
-=======
-    
     
     @RequestMapping("/Shipping")
     public long shippingProduct(@RequestParam long productId){
         productService.getProductById(productId);
         return productId;
     }
-    
-    
->>>>>>> Stashed changes
     
 }
