84f0f70ae8b31d3e7df67fc844ff277389cf651f
Unknown <nutchaphon.eiei@gmail.com>
2018-11-16 22:33:40 +0700

    Omise 70%
    เหลือ price จริงๆ

--- SWprocess/src/main/java/int202/SWprocess/controller/ProductController.java
+++ SWprocess/src/main/java/int202/SWprocess/controller/ProductController.java
@@ -27,11 +27,31 @@ public class ProductController {
     @Autowired
     private ProductService productService;
     
+//    @GetMapping("/product/{id}")
+//    public String getProductById(@PathVariable long id){
+//        return "this is product"+id;
+//    }
+    
     @GetMapping("/index")
     public String getAllProduct(ModelMap model){
         model.addAttribute("allProduct",productService.getAllProducts());
         return "index";
     }
+    
+
+//    @RequestMapping("/index")
+//    public long getProductDetail(@RequestParam long productId){
+//       productService.getProductById(productId);
+//       return productId;
+//    }
+    
+    
+   
+//    @RequestMapping("/index")
+//    public String search(@RequestParam String search){
+//        productService.getAllSearch(search);
+//        return search;
+//    }
 
     @GetMapping("/search")
     public String searchProduct(ModelMap model,@RequestParam String search){
@@ -57,9 +77,6 @@ public class ProductController {
         model.addAttribute("productDetail",productService.getProductById(id));
         return "productdetail";
     }
-    
-    
-//    @RequestMapping("/Shoppingcart")
 
     
 }
