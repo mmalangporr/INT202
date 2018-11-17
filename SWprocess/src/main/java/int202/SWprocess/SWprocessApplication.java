990165478abf343e401576135635921cc80ef74d
nicengaiii <nice.phamin@gmail.com>
2018-11-17 23:56:59 +0700

    Login with Facebook

--- SWprocess/src/main/java/int202/SWprocess/SWprocessApplication.java
+++ SWprocess/src/main/java/int202/SWprocess/SWprocessApplication.java
@@ -1,14 +1,34 @@
 package int202.SWprocess;
 
+import java.security.Principal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @EnableAutoConfiguration
 @SpringBootApplication
-public class SWprocessApplication {
+@EnableOAuth2Sso
+public class SWprocessApplication extends WebSecurityConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SWprocessApplication.class, args);
-	}
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable().antMatcher("/**")
+                .authorizeRequests()           
+                .antMatchers("/Shipping/{id}").authenticated()
+                .anyRequest().permitAll()
+                .and().logout().logoutSuccessUrl("/").permitAll();
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SWprocessApplication.class, args);
+    }
 }
