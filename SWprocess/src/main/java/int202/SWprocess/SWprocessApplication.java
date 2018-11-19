d1aebee937125db67eb32572e337806e390dac63
nicengaiii <nice.phamin@gmail.com>
2018-11-19 21:51:17 +0700

    Remove login with Facebook

--- SWprocess/src/main/java/int202/SWprocess/SWprocessApplication.java
+++ SWprocess/src/main/java/int202/SWprocess/SWprocessApplication.java
@@ -4,30 +4,14 @@ import java.security.Principal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @EnableAutoConfiguration
 @SpringBootApplication
-@EnableOAuth2Sso
-public class SWprocessApplication extends WebSecurityConfigurerAdapter {
 
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable().antMatcher("/**")
-                .authorizeRequests()           
-                .antMatchers("/Shipping/{id}").authenticated()
-                .anyRequest().permitAll()
-                .and().logout().logoutSuccessUrl("/").permitAll();
-    }
-
-    @RequestMapping("/user")
-    public Principal user(Principal principal) {
-        return principal;
-    }
-
+public class SWprocessApplication {
+    
     public static void main(String[] args) {
         SpringApplication.run(SWprocessApplication.class, args);
     }
